396236292a536049771df098facc7a1f03a5f11e
James Humphreys <binaryjim@icloud.com>
2020-04-12 16:34:47 +0100

    #3 Added tests for CounterOutput component

---
+++ src/components/counter-output/counter-output.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import { CounterOutput } from "./counter-output";
+
+jest.mock("../../store", () => ({
+  useCounterStateContext: jest.fn().mockReturnValue(0)
+}))
+
+// Test render
+describe("CounterOutput - test render", () => {
+  it("should render without crashing", () => {
+    shallow(<CounterOutput />);
+  });
+});
+
+// Testing output of the render
+describe("CounterOutput - render", () => {
+  let component;
+
+  beforeAll(() => {
+    component = shallow(<CounterOutput />)
+  })
+
+  it("should match snapshot", () => {
+    expect(toJson(component)).toMatchSnapshot();
+  });
+
+  
+  it('should output the correct count value passed in by context', () => {
+    expect(component.find('.counter-output__count').text()).toEqual("0");
+  })
+})
