4db2818ad8d907b67b009e59795b4bbea6d998fc
James Humphreys <BinaryJim@users.noreply.github.com>
2020-05-19 17:09:58 +0000

    Updates

---
+++ precache-manifest.15b0c20a73a71c31bb32a528047c7b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18aa67983adbc6f8470313b576cba5c0",
+    "url": "/react-counter-app/index.html"
+  },
+  {
+    "revision": "4823ce3ca0b5bbf074ec",
+    "url": "/react-counter-app/static/css/main.db70403d.chunk.css"
+  },
+  {
+    "revision": "c289aa25af5fdf0a9e55",
+    "url": "/react-counter-app/static/js/2.55e50311.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-counter-app/static/js/2.55e50311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4823ce3ca0b5bbf074ec",
+    "url": "/react-counter-app/static/js/main.2259830b.chunk.js"
+  },
+  {
+    "revision": "f4f070645e47011018b3",
+    "url": "/react-counter-app/static/js/runtime-main.4a0955a6.js"
+  }
+]);
