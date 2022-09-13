fcf819edd10047ea598b2a4cec762d7f9a3f28dd
James Humphreys <BinaryJim@users.noreply.github.com>
2022-09-13 18:47:49 +0100

    #21: Add end-to-end tests for app

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,36 @@
+describe("React Counter App", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+
+    cy.findByLabelText("Count value").as("countValue");
+    cy.findByRole("button", { name: "Increment" }).as("incrementValue");
+    cy.findByRole("button", { name: "Decrement" }).as("decrementValue");
+    cy.findByRole("button", { name: "Reset" }).as("resetValue");
+  });
+
+  it("loads with the correct initial count value", () => {
+    cy.get("@countValue").should("contain", "0");
+  });
+
+  it("allows a user to change the count value", () => {
+    cy.get("@incrementValue").click();
+    cy.get("@countValue").should("contain", "1");
+    cy.get("@decrementValue").click();
+    cy.get("@countValue").should("contain", "0");
+  });
+
+  it("allows a user to reset the count value when above zero", () => {
+    cy.get("@incrementValue").click();
+    cy.get("@countValue").should("contain", "1");
+    cy.get("@resetValue").click();
+    cy.get("@countValue").should("contain", "0");
+  });
+
+  it("prevents a user reseting the count value when zero", () => {
+    cy.get("@resetValue").should("be.disabled");
+  });
+
+  it("prevents a user decrementing the count below zero", () => {
+    cy.get("@decrementValue").should("be.disabled");
+  });
+});
