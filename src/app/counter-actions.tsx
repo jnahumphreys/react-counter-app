f187992a82f2e38ee4ee9b7c572176e605353c32
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-01-24 14:22:21 +0000

    #49 - relocate counter context calls within CounterActions

--- src/app/counter-actions.tsx
+++ src/app/counter-actions.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { memo } from "react";
 import styled from "@emotion/styled";
 import { useCounterState, useCounterActions } from "./counter-provider";
 import { Button } from "./button";
+
+function DecrementCount() {
+  const count = useCounterState();
+  const { decrement } = useCounterActions();
+
+  return (
+    <Button
+      disabled={count <= 0}
+      onClick={decrement}
+      aria-label="Decrement"
+      data-cy="decrement"
+    >
+      -
+    </Button>
+  );
+}
+
+function IncrementCount() {
+  const { increment } = useCounterActions();
+
+  return (
+    <Button onClick={increment} aria-label="Increment" data-cy="increment">
+      +
+    </Button>
+  );
+}
+
+const ResetCountButton = memo(
+  styled(Button)({
+    backgroundColor: "#bb2c22",
+    flexBasis: "100%",
+  })
+);
+
+function ResetCount() {
+  const count = useCounterState();
+  const { reset } = useCounterActions();
+
+  return (
+    <ResetCountButton disabled={!(count > 0)} onClick={reset} data-cy="reset">
+      Reset
+    </ResetCountButton>
+  );
+}
 
 const Container = styled.div({
   display: "flex",
@@ -12,33 +56,12 @@ const Container = styled.div({
   margin: "0 auto",
 });
 
-const ResetCountButton = styled(Button)({
-  backgroundColor: "#bb2c22",
-  flexBasis: "100%",
-});
-
 function CounterActions() {
-  const count = useCounterState();
-  const { increment, decrement, reset } = useCounterActions();
-
   return (
     <Container>
-      <Button
-        disabled={count <= 0}
-        onClick={decrement}
-        aria-label="Decrement"
-        data-cy="decrement"
-      >
-        -
-      </Button>
-
-      <Button onClick={increment} aria-label="Increment" data-cy="increment">
-        +
-      </Button>
-
-      <ResetCountButton disabled={!(count > 0)} onClick={reset} data-cy="reset">
-        Reset
-      </ResetCountButton>
+      <DecrementCount />
+      <IncrementCount />
+      <ResetCount />
     </Container>
   );
 }
