c63f55c2c350f57749e050b37eb0064f2f2575b6
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-01-24 16:52:43 +0000

    #52 - Enable fallback for when local storage is not available

--- src/app/counter-provider.tsx
+++ src/app/counter-provider.tsx
@@ -41,13 +41,21 @@ function counterReducer(
 const LOCAL_STORAGE_KEY = "binaryJimCounterAppValue" as const;
 
 function setLocalStorageCountValue(value: number) {
-  localStorage.setItem(LOCAL_STORAGE_KEY, value.toString());
+  try {
+    localStorage.setItem(LOCAL_STORAGE_KEY, value.toString());
+  } catch (error) {
+    return false;
+  }
 }
 
 function getLocalStorageCountValue() {
-  const localStorageValue = localStorage.getItem(LOCAL_STORAGE_KEY);
+  try {
+    const localStorageValue = localStorage.getItem(LOCAL_STORAGE_KEY);
 
-  return localStorageValue ? Number(localStorageValue) : false;
+    return localStorageValue ? Number(localStorageValue) : false;
+  } catch (error) {
+    return false;
+  }
 }
 
 function initCounterReducerState(initialValue: number): CounterReducerState {
