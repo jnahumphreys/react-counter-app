eb6e8b7b6b4776488556b5536f03c88fd6e10645
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-02-01 17:14:41 +0000

    #53 - add Tailwind prettier plugin

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+/* eslint-env node */
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx"],
+  tailwindConfig: "tailwind.config.ts",
+  pluginSearchDirs: false,
+};
