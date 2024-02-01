04a67a349adb61ea869d51f2c080ae9bf11f02e3
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-02-01 14:46:32 +0000

    #59 - remove cra dependency, setup Typescript and ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "jsx-a11y"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  ignorePatterns: [".eslintrc.cjs", "tsconfig.json"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
