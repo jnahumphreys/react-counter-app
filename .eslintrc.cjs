be2c9fc8aadb12a6c3f8e4e8f78026ce074b5ab4
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-06-19 17:59:05 +0000

    #59: enable ESLint post removal of CRA

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  ignorePatterns: [".eslintrc.cjs", "tsconfig.json"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
