328f61e040f97fbbbc40e661d0d0f7e7ffe681aa
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2024-06-20 10:39:26 +0100

    Migrate from Create React App to Parcel - #minor

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  ignorePatterns: [".eslintrc.cjs", "tsconfig.json"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
+};
